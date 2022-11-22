da66576d5ff753418a1cf795b506e1cf570a05fc
Irina <bezdnaya@mail.ru>
2022-11-22 16:12:51 +0200

    Task 4 even numers from 1 to N

---
+++ Task4_EvensTillToN/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine ("Введите целое число");
+int N = Convert.ToInt32(Console.ReadLine());
+int index = 2;
+while (index <= N)
+{
+    Console.Write(index+ " ");
+index = index + 2;
+}
