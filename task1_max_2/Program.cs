a9ba4603050bc090dd5260fa92f643aa456a1796
Irina <bezdnaya@mail.ru>
2022-11-22 15:05:30 +0200

    Task 1 max from 2 numbers

---
+++ task1_max_2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите 2 целых числа ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.WriteLine("max = " + a);
+}
+else {
+    Console.WriteLine("max = " + b);
+}
