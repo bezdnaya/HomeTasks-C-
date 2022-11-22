140f3ab46173df98168a43454ee6097dcb71e1c2
Irina <bezdnaya@mail.ru>
2022-11-22 15:43:20 +0200

    Task 2 max from 3 numbers01~

---
+++ Task2_max_3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите 3 целых числа ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max) max = b;
+if ( c > max) max = c;
+Console.WriteLine("max = " + max);   
