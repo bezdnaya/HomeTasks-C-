922241f6fd345a8c7b8829bae2e5905431c0d3f8
Irina <bezdnaya@mail.ru>
2022-11-24 11:22:47 +0200

    Returning 2nd figure in number

---
+++ task1_Return2ndFigure/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine ("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+number = number - (number/100)*100;
+Console.WriteLine($"Вторая цифра числа: {number / 10}");
